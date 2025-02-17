0b45d0721975b4cf9ae36c6be5760628cd7d090b
kajiki3 <masachikanishikawa@gmail.com>
2025-02-17 21:05:51 +0900

    feat:uv追加

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # hardware-backend
+
+## uvの使い方
+
+pythonの実行
+```sh
+uv run hello.py
+```
+パッケージの追加
+
+```sh
+uv add numpy
+```
---
+++ hello.py
@@ -0,0 +1,8 @@
+import numpy as np
+def main():
+    print("Hello from hardware-backend!")
+
+
+if __name__ == "__main__":
+    main()
+    print(np.random.rand(4,4))
