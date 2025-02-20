9c2bc668288c8191b496b1571bcd0f79bd8c718e
Omusubi0123 <Omusubi0123@github.com>
2025-02-20 11:43:01 +0900

    feat: router調整

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from router.crud import crud_router
 
 app = FastAPI()
+
+app.include_router(crud_router)
 
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, Hardware Backend!!"}
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
--- router/crud.py
+++ router/crud.py
@@ -1,22 +1,23 @@
-from fastapi import Depends
+from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import db.crud as crud
 from db.database import get_db
 from db.schemas import UserCreate, UserResponse
-from main import app
+
+crud_router = APIRouter()
 
 
-@app.post("/users/", response_model=UserResponse)
+@crud_router.post("/users/create/", response_model=UserResponse)
 async def create_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
     return await crud.create_user(db, user)
 
 
-@app.get("/users/", response_model=list[UserResponse])
+@crud_router.get("/users/get/", response_model=list[UserResponse])
 async def get_users(db: AsyncSession = Depends(get_db)):
     return await crud.get_users(db)
 
 
-@app.delete("/users/{user_id}")
+@crud_router.delete("/users/delete/{user_id}")
 async def delete_user(user_id: int, db: AsyncSession = Depends(get_db)):
     return await crud.delete_user(db, user_id)
